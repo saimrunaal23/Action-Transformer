2ef9e3c85390f6cbbf05a9c21966ac2693862db1
saimrunaal23 <96788979+saimrunaal23@users.noreply.github.com>
2022-09-15 18:03:56 -0700

    Add files via upload

--- AcT/utils/trainer_1.py
+++ AcT/utils/trainer_1.py
@@ -1,83 +1,88 @@
+import argparse
 import os
 import torch
 import torchmetrics
 import torch.nn.functional as F
-
 from torch import nn
-from torchvision import transforms
-from torch.utils.data import DataLoader, random_split
+import torch.utils
 
+from utils.tools import read_yaml
+from torch.utils.data import DataLoader, random_split
+from sklearn.model_selection import train_test_split
+
+from utils import data
+from utils.tools import CustomSchedule, CosineSchedule
+from utils.data import load_mpose, random_flip, random_noise, one_hot
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from utils.transformer import TransformerEncoder, PatchClassEmbedding, Patches
 
 
-from PIL import Image
+parser = argparse.ArgumentParser(description='Process some input')
+parser.add_argument('--config', default='config.yaml', type=str, help='Config path', required=False)
+args = parser.parse_args()
+config = read_yaml(args.config)
+
+X_train, y_train, X_test, y_test = load_mpose(config['DATASET'], 1, verbose=False)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                  test_size=config['VAL_SIZE'],
+                                                  random_state=config['SEEDS'][0],
+                                                  stratify=y_train)
+X_train = torch.Tensor(X_train)
+y_train = torch.Tensor(y_train)
+X_val = torch.Tensor(X_val)
+y_val = torch.Tensor(y_val)
+
+ds_train = torch.utils.data.TensorDataset(X_train, y_train)
+
+ds_val = torch.utils.data.TensorDataset(X_val, y_val)
+
 
 class LambdaLayer(nn.Module):
     def __init__(self, lambd):
         super(LambdaLayer, self).__init__()
         self.lambd = lambd
+
     def forward(self, x):
         return self.lambd(x)
 
+
 class LitModel(pl.LightningModule):
 
-    def __init__(self, config, logger):
+    def __init__(self, config):
         self.config = config
-        self.logger = logger
         self.split = 1
         self.fold = 0
         self.trial = None
         self.bin_path = self.config['MODEL_DIR']
-        
+
         self.model_size = self.config['MODEL_SIZE']
         self.n_heads = self.config[self.model_size]['N_HEADS']
         self.n_layers = self.config[self.model_size]['N_LAYERS']
         self.embed_dim = self.config[self.model_size]['EMBED_DIM']
         self.dropout = self.config[self.model_size]['DROPOUT']
         self.mlp_head_size = self.config[self.model_size]['MLP']
-        self.activation = tf.nn.gelu
         self.d_model = 64 * self.n_heads
         self.d_ff = self.d_model * 4
-        self.learning_rate = CustomSchedule(self.d_model, 
-                                warmup_steps=len(self.ds_train)*self.config['N_EPOCHS']*self.config['WARMUP_PERC'],
-                                decay_step=len(self.ds_train)*self.config['N_EPOCHS']*self.config['STEP_PERC'])
-
-
-    def get_data(self):
-        X_train, y_train, X_test, y_test = load_mpose(self.config['DATASET'], self.split, verbose=False)
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
-                                                          test_size=self.config['VAL_SIZE'],
-                                                          random_state=self.config['SEEDS'][self.fold],
-                                                          stratify=y_train)
-                
-        ds_train =  data.TensorDataset((X_train, y_train))
-        ds_train = ds_train.map(lambda x,y : one_hot(x,y,self.config['CLASSES']))
-        ds_train = ds_train.map(random_flip)
-        ds_train = ds_train.map(random_noise)
-        self.ds_train = ds_train.shuffle(X_train.shape[0])
-
-        ds_val = data.TensorDataset((X_val, y_val))
-        self.ds_val = ds_val.map(lambda x,y : one_hot(x,y,self.config['CLASSES']))
-
-
-        ds_test = data.TensorDataset((X_test, y_test))
-        self.ds_test = ds_test.map(lambda x,y : one_hot(x,y,self.config['CLASSES']))
+        self.learning_rate = CustomSchedule(self.d_model,
+                                            warmup_steps=len(ds_train) * self.config['N_EPOCHS'] * self.config[
+                                                'WARMUP_PERC'],
+                                            decay_step=len(ds_train) * self.config['N_EPOCHS'] * self.config[
+                                                'STEP_PERC'])
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(dataset = self.ds_train, batch_size = self.config['BATCH_SIZE'], shuffle=True)
+        return torch.utils.data.DataLoader(dataset=self.ds_train, batch_size=self.config['BATCH_SIZE'], shuffle=True)
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(dataset = self.ds_val, batch_size = self.config['BATCH_SIZE'], shuffle=False)
+        return torch.utils.data.DataLoader(dataset=self.ds_val, batch_size=self.config['BATCH_SIZE'], shuffle=False)
 
     def cross_entropy_loss(self, logits, labels):
-     return F.nll_loss(logits, labels)
+        return F.nll_loss(logits, labels)
 
     def training_step(self, batch, batch_idx):
         data, label = batch
         output = self.forward(data)
-        loss = nn.CrossEntropyLoss()(output,label)
+        loss = nn.CrossEntropyLoss()(output, label)
         self.log('train_loss', loss)
         self.log('train_acc_step', self.accuracy(output, label))
         return {'loss': loss, 'log': self.log}
@@ -96,25 +101,26 @@ class LitModel(pl.LightningModule):
         self.log('val_acc_epoch', self.accuracy.compute())
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=(self.learning_rate))
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
     def forward(self, x):
-       transformer = TransformerEncoder(self.d_model, self.n_heads, self.d_ff, self.dropout, self.activation, self.n_layers)
-       inputs = tf.keras.layers.Input(shape=(self.config['FRAMES'], 
-                                              self.config[self.config['DATASET']]['KEYPOINTS']*self.config['CHANNELS']))
-        x = nn.Linear(self.d_model)(inputs)
+        transformer = nn.Transformer(self.d_model, self.n_heads, self.n_layers,self.n_layers, self.d_ff, self.dropout, nn.GELU())
+      #  inputs = tf.keras.layers.Input(shape=(self.config['FRAMES'],
+       #                                       self.config[self.config['DATASET']]['KEYPOINTS'] * self.config[
+       #                                           'CHANNELS']))
+        x = nn.Linear(self.d_model)(x)
         x = PatchClassEmbedding(self.d_model, self.config['FRAMES'])(x)
         x = transformer(x)
-        x = LambdaLayer(lambda x: x[:,0,:])(x)
+        x = LambdaLayer(lambda x: x[:, 0, :])(x)
         x = nn.Linear(self.mlp_head_size)(x)
         outputs = nn.Linear(self.config['CLASSES'])(x)
 
         return outputs
 
 
-model = LitModel(batch_size = 32, learning_rate=0.001)
+model = LitModel(config)
 
-trainer = pl.Trainer(gpus=1, self.config['N_EPOCHS'])
+trainer = pl.Trainer(accelerator='cpu', max_epochs=2)
 
 trainer.fit(model)
